0f184993461a1b0257b847b425b4826257c23318
dave <djthorne@edu.uwaterloo.ca>
2018-10-01 17:36:44 -0400

    rendering sparkline weather charts, embed google map for each city searched

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const API_KEY='c05d36744be52bc85010a65c7a493f93';
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+ export function fetchWeather(city) {
+     const url = `${ROOT_URL}&q=${city},us`;
+     const request = axios.get(url);
+     return {
+        type: FETCH_WEATHER,
+        payload: request
+     };
+ }
